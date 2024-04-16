2898e4626211a2759215624781bfc1d80a71210e
JerB <143885597+JerB9@users.noreply.github.com>
2024-04-16 09:20:24 -0400

    Update Circuit-LAN3-Sono.py

--- Circuit-LAN3-Sono.py
+++ Circuit-LAN3-Sono.py
@@ -25,6 +25,9 @@ except pd.errors.ParserError as e:
     print("ParserError:", e)
     # Gestion des erreurs de format et affichage d'un message d'erreur
 
+# Transformation des valeurs négatives en 0
+
+
 # Transformation du format des secondes en entier (arrondissement à la seconde)
 df['Second'] = df['Second'].astype(int)
 
